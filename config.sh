8c601cd71e3bfb5aaad412b560c8212f30af0cc2
Bryan Lokey <email@bryanlokey.com>
2023-02-13 11:59:23 -0700

    Fix typo

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This is where you setup variables used in these scripts
-organization="oragnization-slug-here"
+organization="organization-slug-here"
 resultsLocation='./results'
 detailedResultsLocation="${resultsLocation}/pr-list"
 prCreatedDate=">2021-08-01"
